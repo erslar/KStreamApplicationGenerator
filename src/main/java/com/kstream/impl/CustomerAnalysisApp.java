aafcc512dfa555bcff6ded122d77fc9051f9966d
nishutayal <nishutayal@gmail.com>
2018-01-29 14:23:38 +0000

    added join operation

--- src/main/java/com/kstream/impl/CustomerAnalysisApp.java
+++ src/main/java/com/kstream/impl/CustomerAnalysisApp.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.apache.avro.Schema;
@@ -14,6 +15,7 @@ import org.apache.avro.generic.GenericArray;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.Encoder;
 import org.apache.avro.io.EncoderFactory;
 import org.apache.avro.io.JsonEncoder;
@@ -49,27 +51,26 @@ import io.confluent.kafka.serializers.KafkaAvroDeserializer;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.streams.serdes.avro.GenericAvroSerde;
 
+/**
+ * Generic Pipeline to join two kafka topics
+ * 
+ * @author nishu.tayal
+ *
+ */
 public class CustomerAnalysisApp {
 
 	public static Logger logger = LoggerFactory.getLogger(CustomerAnalysisApp.class);
-	
-	static class EntityAccumulator{
-		static ArrayList<GenericRecord> list = new ArrayList<GenericRecord>();
-		
-		public EntityAccumulator add(GenericRecord gr){
-			list.add(gr);
-			return this;
-		}
-	}
 
 	public static void main(String[] args) {
-		
+
 		EntityOperation entityOperation = new EntityOperation<>();
 
 		Properties customProperties = PropertyUtil.getProperties(Constants.PROPERTIES_FILE);
 		String BOOTSTRAP_SERVERS_CONFIG = customProperties.getProperty("bootstrap.servers");
 		String ZOOKEEPER_CONNECT_CONFIG = customProperties.getProperty("zookeeper.url");
 		String SCHEMA_REGISTRY_URL_CONFIG = customProperties.getProperty("schema.registry.url");
+		String TOPIC1_LABEL = customProperties.getProperty("topic1Label");
+		String TOPIC2_LABEL = customProperties.getProperty("topic2Label");
 
 		String methodName = new Object() {
 		}.getClass().getEnclosingMethod().getName();
@@ -80,7 +81,6 @@ public class CustomerAnalysisApp {
 
 		String firstTopic = customProperties.getProperty("topic1");
 		String secondTopic = customProperties.getProperty("topic2");
-		String thirdTopic = "testtopic";
 
 		System.out.println(firstTopic);
 		System.out.println(secondTopic);
@@ -100,8 +100,6 @@ public class CustomerAnalysisApp {
 			streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS_CONFIG);
 			streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, ZOOKEEPER_CONNECT_CONFIG);
 			streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
-			// streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG,
-			// Serdes.ByteArray().getClass().getName());
 			streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, GenericAvroSerde.class);
 			streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
 			streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
@@ -117,133 +115,23 @@ public class CustomerAnalysisApp {
 
 			Serde<GenericRecord> avroSerde = Serdes.serdeFrom(kafkaAvroSerializer, kafkaAvroDeserializer);
 
-			
-			Serializer arrayListSerializer = new ArrayListSerializer<GenericRecord>();
-			Deserializer arrayListDeserializer = new ArrayListDeserializer<GenericRecord>(kafkaAvroDeserializer);
-			
-			Serde<ArrayList<GenericRecord>> arrayListSerde = Serdes.serdeFrom(kafkaAvroSerializer, kafkaAvroDeserializer);
-
+			Serde outputKeySerde = Serdes.ByteArray();
 			// Read the kafka topics
 			StreamsBuilder builder = new StreamsBuilder();
 
 			KStream<byte[], GenericRecord> firstStream = builder.stream(firstTopic);
 			KStream<byte[], GenericRecord> secondStream = builder.stream(secondTopic);
-		 	
-			KTable<byte[], Map<String, ArrayList<GenericRecord>>> firstTable = entityOperation.groupAndAggregateLabel(firstStream, avroSerde, firstStorage, "customerRecords");
-			KTable<byte[], Map<String,ArrayList<GenericRecord>>> secondTable = entityOperation.groupAndAggregateLabel(secondStream, avroSerde, secondStorage,"policyRecords");
-			
-		//	KTable<byte[], ArrayList<GenericRecord>> secondTable = entityOperation.groupAndAggregate(secondStream, avroSerde, secondStorage);
-			
-			firstTable.print();
-		 	secondTable.print();
-		 	 
-		   KTable<byte[], Object> joinedKTable =
-			   firstTable.join(secondTable, (value1, value2) -> {
-				//   List<HashMap<String,ArrayList<GenericRecord>>> list = new ArrayList<>();
-				   List list = new ArrayList<>();
-				   list.add(value1);
-				   list.add(value2);
-				   System.out.println("//----------printing JOIN LIST----------------");
-				   System.out.println(list);
-				   System.out.println(list.size());
-				   System.out.println(list.get(0));
-				   System.out.println(list.get(1));
-				   System.out.println("//---------- LIST ENDS HERE--------------");
-				   return list;
-			   });
- 	  
-			// System.out.println(joinedKTable);
-			// logger.info(joinedKTable.toString());
 
-			String schema = "{\"namespace\":\"com.test\",\"name\":\"customerView\",\"type\":\"array\",\"items\":{\"name\":\"customer\",\"type\":\"record\",\"fields\":[{\"name\":\"name\",\"type\":\"string\"}]}}";
-		
+			KTable<byte[], Map<String, ArrayList<GenericRecord>>> firstTable = entityOperation
+					.groupAndAggregateLabel(firstStream, avroSerde, firstStorage, TOPIC1_LABEL);
+			KTable<byte[], Map<String, ArrayList<GenericRecord>>> secondTable = entityOperation
+					.groupAndAggregateLabel(secondStream, avroSerde, secondStorage, TOPIC2_LABEL);
 
-			String schema3 = "{\"namespace\":\"com.test\",\"name\":\"customerView2\",\"type\":\"record\",\"fields\":[{\"name\":\"customerRecords\",\"type\": { \"type\":\"array\", \"items\": { \"name\": \"customer\",\"type\":\"map\",\"values\" :\"string\" }}},{\"name\":\"policyRecords\",\"type\": { \"type\":\"array\", \"items\": { \"name\": \"policy\",\"type\":\"map\",\"values\" :\"string\"}}}]}";
+			// writing data in GenericRecord format
+			KTable<byte[], GenericRecord> joinedKTable = entityOperation.join(firstTable, secondTable,
+					Constants.OUTPUT_SCHEMA1);
 
-			
-	//		final Schema avroSchema = new Schema.Parser().parse(schema3);
-
-	 		
-			final Schema avroSchema2 = new Schema.Parser().parse(schema);
-			final Schema avroSchema3 = new Schema.Parser().parse(schema3);
-
-
-			final EncoderFactory encoderFactory = EncoderFactory.get();
-
-			//TODO : development in progress
-	//		KTable<byte[],ArrayList<GenericRecord>> joinedKTable1 = firstTable.join(secondTable);
-			
-	/*		
-			KTable<byte[], byte[]> joinedKTable = firstTable.join(secondTable,
-					new ValueJoiner<GenericRecord, GenericRecord, byte[]>() {
-						@Override
-						public byte[] apply(GenericRecord l, GenericRecord r) {
-							System.out.println(l);
-							System.out.println(r);
-							GenericArray<GenericRecord> avroArray = new GenericData.Array<GenericRecord>(10,
-									avroSchema);
-							avroArray.add(l);
-							avroArray.add(r);
-							// final byte[] dt1 =
-							// System.out.println(avroArray.getSchema());
-							System.out.println("========= avroArray string version=======");
-							String s = avroArray.toString();
-							System.out.println(s);
-							
-							GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<>(avroSchema3);
-							ByteArrayOutputStream out = new ByteArrayOutputStream();
-							Encoder binaryEncoder = encoderFactory.binaryEncoder(out, null);
-					//		JsonEncoder encoder 
-							try {
-								GenericRecord gr = new GenericData.Record(avroSchema3);
-								gr.put("data",s);
-								 
-								writer.write( gr, binaryEncoder);
-								binaryEncoder.flush();
-								out.close();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-							System.out.println("========= out variable in string version=======");
-							System.out.println(out);
-							System.out.println(out.toByteArray());
-							return out.toByteArray();
-
-							// System.out.println(avroArray);
-							// System.out.println(avroArray);
-
-						}
-					} );
-			
-			/*
-			 * KTable<GenericRecord, GenericArray<GenericRecord>> joinedKTable3
-			 * = firstTable.join(secondTable, new ValueJoiner<GenericRecord,
-			 * GenericRecord, GenericArray<GenericRecord>>() {
-			 * 
-			 * @Override public GenericArray<GenericRecord> apply(GenericRecord
-			 * l, GenericRecord r) { GenericArray<GenericRecord> avroArray = new
-			 * GenericData.Array<GenericRecord>(10, avroSchema);
-			 * avroArray.add(l); avroArray.add(r); //
-			 * System.out.println(avroArray); return avroArray; } });
-			 */
-
-			// (customer, policy) -> new CustomerAndPolicy(customer, policy));
-
-			/*
-			 * KStream<GenericRecord, List<GenericRecord>> outputStream =
-			 * firstStream.join(secondStream, new ValueJoiner<GenericRecord,
-			 * GenericRecord, List<GenericRecord>>() {
-			 * 
-			 * @Override public List<GenericRecord> apply(GenericRecord l,
-			 * GenericRecord r) { List<GenericRecord> gList = new
-			 * ArrayList<GenericRecord>(); gList.add(l); gList.add(r); return
-			 * gList; } }, JoinWindows.of(TimeUnit.SECONDS.toMillis(5)),
-			 * avroSerde, avroSerde, avroSerde);
-			 * 
-			 */
-		 //	 joinedKTable2.toStream().to(outputTopic,Produced.with(Serdes.ByteArray(),
-		 //	 Serdes.ByteArray()));
+			joinedKTable.toStream().to(outputTopic, Produced.with(outputKeySerde, avroSerde));
 
 			KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
 			streams.cleanUp();
@@ -258,3 +146,4 @@ public class CustomerAnalysisApp {
 
 	}
 }
+
--- src/main/java/com/kstream/operations/EntityOperation.java
+++ src/main/java/com/kstream/operations/EntityOperation.java
@@ -1,12 +1,21 @@
 package com.kstream.operations;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Stream;
 
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.EncoderFactory;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.Joined;
@@ -14,6 +23,7 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.ValueJoiner;
 
+import com.kstream.util.Constants;
 import com.kstream.util.serde.hashmap.HashMapSerde;
 import com.kstream.util.serde.list.ArrayListSerde;
 
@@ -44,17 +54,139 @@ public class EntityOperation<K, V, T, VR> {
 				}, new ArrayListSerde(serdeType), storageName);
 
 		KTable<K, Map<String, ArrayList<V>>> labledGroupedTable = groupedTable.mapValues(value -> {
-			Map<String,ArrayList<V>> map = new HashMap<String,ArrayList<V>>();
+			Map<String, ArrayList<V>> map = new HashMap<String, ArrayList<V>>();
 			map.put(labelName, value);
 			return map;
 		});
 		return labledGroupedTable;
 	}
 
-	public <VO, VR> KStream<K, VR> join(final KStream<K, VO> otherStream,
-			final ValueJoiner<? super V, ? super VO, ? extends VR> joiner, final JoinWindows windows,
-			final Joined<K, V, VO> joined) {
-		return null;
+	/**
+	 * Join two Ktables
+	 * 
+	 * @param table1
+	 * @param table2
+	 * @param schema
+	 * @return KTable<K,GenericRecord>
+	 */
+	public <V1, V2> KTable<K, GenericRecord> join(KTable<K, V1> table1, KTable<K, V2> table2, String schema) {
+		// writing data in GenericRecord format
+		final Schema avroSchema = new Schema.Parser().parse(schema);
+		GenericRecord record = new GenericData.Record(avroSchema);
+		KTable<K, GenericRecord> joinedKTable = table1.join(table2, new ValueJoiner<V1, V2, GenericRecord>() {
+			@Override
+			public GenericRecord apply(V1 value1, V2 value2) {
+				// TODO Auto-generated method stub
+				System.out.println("//----------------------- Inside Join apply method---------------//");
+				System.out.println("//--- Printing Value1 and Value2-------//");
+				System.out.println(value1);
+				System.out.println(value2);
+				try {
+					if (value1 instanceof Map<?, ?>) {
+						for (Entry<?, ?> entry : ((Map<?, ?>) value1).entrySet()) {
+							System.out.println(entry);
+							String key = (String) entry.getKey();
+							Object entryValue = entry.getValue();
+							System.out.println(key);
+							System.out.println(entryValue);
+							record.put(key, entryValue);
+						}
+					}
+					if (value2 instanceof Map<?, ?>) {
+						System.out.println("//----------------- Inside value2 if statement-----------//");
+						System.out.println(value2);
+						System.out.println("//------------------- above is the value----------------//");
+						for (Entry<?, ?> entry : ((Map<?, ?>) value2).entrySet()) {
+							System.out.println(entry);
+							String key = (String) entry.getKey();
+							Object entryValue = entry.getValue();
+							System.out.println(key);
+							System.out.println(entryValue);
+							record.put(key, entryValue);
+						}
+					}
+					System.out.println("//--------------- Here is the Record---------//");
+					System.out.println(record);
+					return record;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
+
+			}
+
+		});
+		return joinedKTable;
 	}
 
+	/**
+	 * Join two Ktables
+	 * 
+	 * @param table1
+	 * @param table2
+	 * @param schema
+	 * @return KTable<K,byte[]>
+	 */
+	public <V1, V2> KTable<K, byte[]> join2(KTable<K, V1> table1, KTable<K, V2> table2, String schema) {
+		// writing data in GenericRecord format
+
+		final Schema avroSchema = new Schema.Parser().parse(schema);
+		GenericDatumWriter<GenericRecord> writer = new GenericDatumWriter<>(avroSchema);
+		final EncoderFactory encoderFactory = EncoderFactory.get();
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		BinaryEncoder encoder = encoderFactory.binaryEncoder(out, null);
+		GenericRecord record = new GenericData.Record(avroSchema);
+		KTable<K, byte[]> joinedKTable = table1.join(table2, new ValueJoiner<V1, V2, byte[]>() {
+			@Override
+			public byte[] apply(V1 value1, V2 value2) {
+				// TODO Auto-generated method stub
+				System.out.println("//----------------------- Inside Join apply method---------------//");
+
+				System.out.println("//--- Printing Value1 and Value2-------//");
+				System.out.println(value1);
+				System.out.println(value2);
+				try {
+					if (value1 instanceof Map<?, ?>) {
+						for (Entry<?, ?> entry : ((Map<?, ?>) value1).entrySet()) {
+							System.out.println(entry);
+							String key = (String) entry.getKey();
+							Object entryValue = entry.getValue();
+							System.out.println(key);
+							System.out.println(entryValue);
+							// ArrayList<GenericRecord> list = value1.get(key);
+							record.put(key, entryValue);
+						}
+					}
+					if (value2 instanceof Map<?, ?>) {
+						System.out.println("//----------------- Inside value2 if statement-----------//");
+						System.out.println(value2);
+						System.out.println("//------------------- above is the value----------------//");
+						for (Entry<?, ?> entry : ((Map<?, ?>) value2).entrySet()) {
+							System.out.println(entry);
+							String key = (String) entry.getKey();
+							Object entryValue = entry.getValue();
+							System.out.println(key);
+							System.out.println(entryValue);
+							// ArrayList<GenericRecord> list = value1.get(key);
+							record.put(key, entryValue);
+						}
+					}
+					System.out.println("//--------------- Here is the Record---------//");
+					System.out.println(record);
+					// Convert GenericRecord into bytes
+					writer.write(record, encoder);
+					encoder.flush();
+					byte[] outBytes = out.toByteArray();
+					return outBytes;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
+
+			}
+
+		});
+		return joinedKTable;
+	}
 }
+
